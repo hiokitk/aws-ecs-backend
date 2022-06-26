2e4fc256669a56241cf42ab03e625196f1619cf3
takahiro hioki <Takahiro.Hioki@jp.nttdata.com>
2022-06-26 21:29:05 +0900

    first commit

---
+++ src/main/java/com/example/backend/app/web/BackendRestController.java
@@ -0,0 +1,21 @@
+package com.example.backend.app.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.example.backend.app.model.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1")
+public class BackendRestController {
+    @GetMapping("/users")
+    public List<User> getUsers(){
+        List<User> users = new ArrayList<>();
+        users.add(User.builder().userId("1").userName("Taro").build());
+        users.add(User.builder().userId("2").userName("Jiro").build());
+        return users;
+    }
+}
---
+++ src/test/java/com/example/backend/BackendApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.backend;
+
+import com.example.backend.config.BackendApplication;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes= BackendApplication.class)
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
